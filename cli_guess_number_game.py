b20eafaf6af1fb003ae0fb3013c227b8e239626d
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-12-28 13:25:16 +0800

    improve user prompts for clarity in the guessing game

--- cli_guess_number_game.py
+++ cli_guess_number_game.py
@@ -1,7 +1,7 @@
 import random
 
 number = random.randint(1, 99)
-name = input("Enter your name: ")
+name = input("\nEnter your name: ")
 max_attempts = int(input("Enter attempt that you want: "))
 attempts = 0
 
@@ -9,15 +9,15 @@ while attempts < max_attempts:
     guess = int(input("Enter a number from 1 to 99: "))
 
     if guess < number:
-        print("Guess is low")
+        print("Your guess is low")
     elif guess > number:
-        print("Guess is high")
+        print("Your guess is high")
     else:
         print(f"Congratulations {name}! You guessed it!")
         break
 
     attempts += 1
-    print(f"Attempts remaining: {max_attempts - attempts}\n")
+    print(f"Attempts remaining: {max_attempts - attempts}")
 else:
     print(f"Out of attempts, You Lost. The number was {number}")
 
@@ -31,10 +31,10 @@ guess = int(input("Enter a number from 1 to 99: "))
 
 while True:
     if guess < number:
-        print("Guess is low")
+        print("Your guess is low")
         guess = int(input("Enter a number from 1 to 99: "))
     elif guess > number:
-        print("Guess is high")
+        print("Your guess is high")
         guess = int(input("Enter a number from 1 to 99: "))
     else:
         print("Congrats, you guessed it!")
